d00b0356f330497fe1347808a03286baf2d8c634
fred <fred.bai@silksoftware.com>
2019-04-23 00:15:31 +0800

    update new api url

--- assets/js/theme/b2b/config.js
+++ assets/js/theme/b2b/config.js
@@ -1,17 +1,8 @@
-// dev
-/*export default {
-	storeHash: "h3jnjw30qw",
-	apiRootUrl: "https://fl4mq0bm40.execute-api.us-west-2.amazonaws.com/prod"
-}*/
-
-// prod
-/*export default {
-	storeHash: "j9bye8pq04",
-	apiRootUrl: "https://8l6t4dsd6e.execute-api.us-west-2.amazonaws.com/prod"
-}*/
-
-
 export default {
 	storeHash: "c91ymf1aq",
-	apiRootUrl: "https://8l6t4dsd6e.execute-api.us-west-2.amazonaws.com/prod"
+
+	// old apiUrl
+	// apiRootUrl: "https://8l6t4dsd6e.execute-api.us-west-2.amazonaws.com/prod"
+	// new apiUrl
+	apiRootUrl: "https://v58yqgkmjb.execute-api.us-west-2.amazonaws.com/prod"
 }
